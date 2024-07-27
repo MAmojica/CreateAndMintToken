360191e11e76898d3db02004c3f754ae8e6ccee7
MAmojica <78264218+MAmojica@users.noreply.github.com>
2024-07-27 20:19:31 +0800

    Update ITokenInterface.sol

--- ITokenInterface.sol
+++ ITokenInterface.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+interface ITokenInterface {
+    function createTokens(address _recipient, uint256 _amount) external;
+    function destroyTokens(uint256 _amount) external;
+    function sendTokens(address _recipient, uint256 _amount) external returns (bool success);
+
+    event TokensSent(address sender, address recipient, uint256 amount);
+}
